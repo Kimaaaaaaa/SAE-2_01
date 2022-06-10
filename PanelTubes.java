09fd769f11ae94ccfa69919039a5bdb1342fcdeb
Kimaaaaaaa <102507341+Kimaaaaaaa@users.noreply.github.com>
2022-06-10 17:26:57 +0200

    Create PanelTubes.java

---
+++ PanelTubes.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.GridLayout;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
+
+public class PanelTubes extends JPanel
+{
+
+	public PanelTubes()
+	{	
+		this.setLayout(new BorderLayout());
+
+		JPanel panelMilieu = new JPanel();
+
+		this.add(panelMilieu);
+	}
+
+
+
+
+}
