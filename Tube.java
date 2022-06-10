2d71b85735a566ab79a680fe49dd49df947af308
Kimaaaaaaa <102507341+Kimaaaaaaa@users.noreply.github.com>
2022-06-10 10:25:44 +0200

    Update Tube.java

--- Tube.java
+++ Tube.java
@@ -1,1 +1,18 @@
 
+public class Tube
+{
+   private int section;
+
+    public Tube(int section)
+    {
+        if (section < 2 || section > 10)
+            return null;
+
+        this.section = section;
+    }
+
+    public int getSection()
+    {
+        return this.section;
+    }
+}
