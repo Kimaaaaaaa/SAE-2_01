34b0c85d04e862f4bd65d926b5ab8849ee53f21a
Kimaaaaaaa <102507341+Kimaaaaaaa@users.noreply.github.com>
2022-06-15 09:13:27 +0200

    Create Controleur.java

---
+++ 1.2/Controleur.java
@@ -0,0 +1,14 @@
+public class Controleur
+{
+    //private FramePrinc ihm;
+
+    public Controleur()
+	{
+		new FramePrinc();
+	}
+
+    public static void main(String[] args)
+    {
+        new Controleur();
+    }
+}
